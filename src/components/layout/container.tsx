c73ac0f53a0ec73e1034e9e5db952927c995d92b
Brandon Chikezie <75492569+Brandon30067308@users.noreply.github.com>
2023-05-25 13:23:20 +0100

    Update

--- src/components/layout/container.tsx
+++ src/components/layout/container.tsx
@@ -3,6 +3,7 @@
 import "aos/dist/aos.css"
 
 import { DetailedHTMLProps, FC, useEffect } from "react"
+import { Inter, Playfair_Display } from "next/font/google"
 import AOS from "aos"
 import classNames from "classnames"
 
@@ -12,6 +13,18 @@ import { Header as HeaderType } from "@/src/payload-types"
 type Props = {
   header: HeaderType
 } & DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+})
+
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair-display",
+  display: "swap",
+})
 
 const PageContainer: FC<Props> = function ({ header, children }) {
   useEffect(() => {
@@ -24,7 +37,13 @@ const PageContainer: FC<Props> = function ({ header, children }) {
   })
 
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
+    <div
+      className={classNames(
+        "flex flex-col min-h-screen overflow-hidden",
+        inter.variable,
+        playfair.variable
+      )}
+    >
       <Header {...header} />
 
       <main className={classNames("grow")}>{children}</main>
